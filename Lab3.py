1ccd04d0368648106ba9bc5c62c36432ee563186
L1oid <71085120+L1oid@users.noreply.github.com>
2023-02-22 14:59:22 +0700

    Update Lab3.py

--- Lab3.py
+++ Lab3.py
@@ -26,6 +26,23 @@ c.create_line(x2, y2, x3, y3)
 c.create_line(x3, y3, x1, y1)
 
 
+def cross(x1, y1, x2, y2, x3, y3, x4, y4):
+    n = 0
+    if y2 - y1 != 0:
+        q = (x2 - x1) / (y1 - y2)
+        sn = (x3 - x4) + (y3 - y4) * q
+        if sn <= 0:
+            return 0, 0
+        fn = (x3 - x1) + (y3 - y1) * q
+        n = fn / sn
+    else:
+        if y3 - y4 <= 0:
+            return 0, 0
+        n = (y3 - y1) / (y3 - y4)
+    xcross = x3 + (x4 - x3) * n
+    ycross = y3 + (y4 - y3) * n
+    return xcross, ycross
+
 def up_triangle():
     global x1, y1, x2, y2, x3, y3
     c.create_line(x1, y1, x2, y2, fill='white')
@@ -36,10 +53,27 @@ def up_triangle():
     y2 -= 5
     y3 -= 5
 
-    if y1 <= topY:
-        c.create_line(x1, topY, x2, y2)
-        c.create_line(x2, y2, x3, y3)
-        c.create_line(x3, y3, x1, topY)
+
+    if y1 <= topY or x2 <= topX:
+        x1new, y1new = cross(x1, y1, x2, y2, topX, topY, botX, topY)
+        if x1new == 0 and y1new == 0:
+            x1new = x1
+            y1new = y1
+        x1new2, y1new2 = cross(x3, y3, x1, y1, topX, topY, botX, topY)
+        if x1new2 == 0 and y1new2 == 0:
+            x1new2 = x1
+            y1new2 = y1
+        x2new, y2new = cross(x1, y1, x2, y2, topX, topY, topX, botY)
+        if x2new == 0 and y2new == 0:
+            x2new = x2
+            y2new = y2
+        x2new2, y2new2 = cross(x2, y2, x3, y3, topX, topY, topX, botY)
+        if x2new2 == 0 and y2new2 == 0:
+            x2new2 = x2
+            y2new2 = y2
+        c.create_line(x1new, y1new, x2new, y2new)
+        c.create_line(x2new2, y2new2, x3, y3)
+        c.create_line(x3, y3, x1new2, y1new2)
     else:
         c.create_line(x1, y1, x2, y2)
         c.create_line(x2, y2, x3, y3)
